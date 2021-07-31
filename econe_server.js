3922b301df0dc5dbe4ac20629575a8c8cd670975
Nao Julius <naojulius.mg@gmail.com>
2021-07-31 08:20:40 +0300

    Base de code à livrer

---
+++ econe_server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+const port = 4020;
+app.use(express.static(__dirname + '/dist/'));
+app.get('/*', (req, res)=>{
+    res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
+
+const server = http.createServer(app);
+server.listen(port, ()=>console.log("Running"));
